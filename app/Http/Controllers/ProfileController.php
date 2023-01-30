1436d42f3983a6ed8e277963c6f0e51971378238
Lythmass <lythmass17@gmail.com>
2023-01-30 16:46:16 +0400

    refactor: move update validaion to custom request

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -13,40 +13,16 @@ class ProfileController extends Controller
         return response(['user-data' => auth()->user()]);
     }
 
-    public function store(Request $request)
+    public function store(StoreUpdateRequest $request)
     {
         $userDetails = Auth::user();
         $user = User::find($userDetails->id);
-        if ($user->name === $request['username'] && strlen($request['password']) < 1) {
-            return response()->json(['message' => 'No changes were made.']);
-        }
-        if ($user->name === $request['username'] && strlen($request['password']) >= 1) {
-            $request->validate([
-                'password' => ['min:8', 'max:15', 'regex:/^[a-z0-9]+$/', 'confirmed'],
-                'password_confirmation' => ['required']
-            ]);
+        $request->validated();
+        if ($request['password'] !== null) {
             $user->password = bcrypt($request['password']);
-            $user->save();
-            return response()->json(['message' => 'Your password was changed successfully!']);
         }
-        if ($user->name !== $request['username'] && strlen($request['password']) < 1) {
-            $request->validate(['username' =>['required', 'min:3', 'max:15', 'regex:/^[a-z0-9]+$/', 'unique:users,name']]);
-            $user->name = $request['username'];
-            $user->save();
-            return response()->json(['message' => 'Your username was changed successfully!']);
-        }
-        if ($user->name !== $request['username'] && strlen($request['password']) >= 1) {
-            $request->validate([
-                'password' => ['min:8', 'max:15', 'regex:/^[a-z0-9]+$/', 'confirmed'],
-                'password_confirmation' => ['required'],
-                'username' =>['required', 'min:3', 'max:15', 'regex:/^[a-z0-9]+$/', 'unique:users,name']
-            ]);
-            $user->password = bcrypt($request['password']);
-            $user->name = $request['username'];
-            $user->save();
-            return response()->json(['message' => 'Your username and password was changed successfully!']);
-        }
-
+        $user->name = $request['username'];
+        $user->save();
         return response()->json(['message' => 'Successfully changed data!']);
     }
 }
---
+++ app/Http/Requests/StoreUpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateRequest extends FormRequest
+{
+    public function rules()
+    {
+        if ($this['username'] === auth()->user()->name) {
+            return [
+                'password' => ['nullable', 'min:8', 'max:15', 'regex:/^[a-z0-9]+$/', 'confirmed'],
+                'password_confirmation' => ['required_if:password,not_null'],
+            ];
+        } else {
+            return [
+                'password' => ['nullable', 'min:8', 'max:15', 'regex:/^[a-z0-9]+$/', 'confirmed'],
+                'password_confirmation' => ['required_if:password,not_null'],
+                'username' =>['required', 'min:3', 'max:15', 'regex:/^[a-z0-9]+$/', 'unique:users,name']
+            ];
+        }
+    }
+}
